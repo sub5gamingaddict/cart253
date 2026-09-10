1ecf8823c01d7f1926c9fb6f414e20e57ac19134
khglazer <w_naim@live.concordia.ca>
2026-09-10 16:55:11 -0400

    changed numbers

--- topics/version-control/version-control-workflow/js/script.js
+++ topics/version-control/version-control-workflow/js/script.js
@@ -26,11 +26,11 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    background(100, 200, 300);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 10;
+    const levels = 30;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -38,7 +38,7 @@ function draw() {
         // Set the grey shade of the level based on its number
         // e.g. level 1 will get a shade of 10 (dark gray), 
         // level 10 will be 255(white)
-        const shade = map(level, 1, levels, 10, 255);
+        const shade = map(level, 10, levels, 25, 367);
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
