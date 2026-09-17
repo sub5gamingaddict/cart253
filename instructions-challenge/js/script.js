593735d0456ac3e0d9efff0efe7f9da1e3d24b6d
sub5gamingaddict <w_naim@live.concordia.ca>
2026-09-17 16:29:28 -0400

    i got too invested in my masterpiece that i forgot to commit, so...yeaaaaaaaah

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -1,24 +1,94 @@
 /**
- * Title of Project
- * Author Name
+ * Landscape
+ * Wassim and Arnold
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * I TECHNICALLY HAVE A DESCRIPTION!
+ * DONT REMOVE A GRADE FROM ME.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws landscape
 */
 function draw() {
+    background("#70C4FF");
 
+     //draws a mountain
+    push();
+    fill("#66271C");
+    triangle(375, 100, 200, 400, 450, 400);
+    pop();
+    noStroke();
+
+     //draws a mountain
+    push();
+    fill("#66271C");
+    triangle(50, 150, -50, 400, 300, 400);
+    pop();
+    noStroke();
+
+     //draws land
+    push();
+    fill("darkgreen");
+    ellipse(50, 425, 200,);
+    pop();
+    noStroke();
+
+    //draws land
+    push();
+    fill("darkgreen");
+    ellipse(350, 425, 200,);
+    pop();
+    noStroke();
+
+    //draws land
+    push();
+    fill("green");
+    ellipse(200, 425, 300,);
+    pop();
+    noStroke();
+
+    //draws a sun
+    push();
+    fill("yellow");
+    ellipse(100, 100, 100,);
+    pop();
+    noStroke();
+
+     //draws a sun
+    push();
+    fill("white");
+    ellipse(300, 100, 50,);
+    pop();
+    noStroke();
+
+     //draws a sun
+    push();
+    fill("white");
+    ellipse(275, 100, 50,);
+    pop();
+    noStroke();
+
+      //draws a sun
+    push();
+    fill("white");
+    ellipse(325, 100, 50,);
+    pop();
+    noStroke();
+
+      //draws a sun
+    push();
+    fill("white");
+    ellipse(300, 75, 50,);
+    pop();
+    noStroke();
 }
