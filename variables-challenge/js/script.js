a8fd63afe637b1b5bc8d8d66ef5130670c8fe34d
sub5gamingaddict <w_naim@live.concordia.ca>
2026-09-24 15:03:19 -0400

    pasted mr furious

--- variables-challenge/js/script.js
+++ variables-challenge/js/script.js
@@ -1,24 +1,43 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+//**
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+};
 
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
-
+  background(160, 180, 200);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 }
